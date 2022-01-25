22512623cefc180f4709177f2e20afd39c70e32e
Jay Heng <jie.heng@nxp.com>
2022-01-25 14:03:13 +0800

    Implement help menu bar for GUI
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- src/main.py
+++ src/main.py
@@ -11,6 +11,25 @@ class memTesterMain(runcore.memTesterRun):
 
     def __init__(self, parent=None):
         super(memTesterMain, self).__init__(parent)
+        self._register_callbacks()
+
+    def _register_callbacks(self):
+        self.menuHelpAction_homePage.triggered.connect(self.callbackShowHomePage)
+        self.menuHelpAction_aboutAuthor.triggered.connect(self.callbackShowAboutAuthor)
+        self.menuHelpAction_revisionHistory.triggered.connect(self.callbackShowRevisionHistory)
+
+    def callbackShowHomePage(self):
+        QMessageBox.about(self, uilang.kMsgLanguageContentDict['homePage_title'][0], uilang.kMsgLanguageContentDict['homePage_info'][0] )
+
+    def callbackShowAboutAuthor(self):
+        msgText = ((uilang.kMsgLanguageContentDict['aboutAuthor_author'][0]) +
+                   (uilang.kMsgLanguageContentDict['aboutAuthor_email1'][0]) +
+                   (uilang.kMsgLanguageContentDict['aboutAuthor_email2'][0]) +
+                   (uilang.kMsgLanguageContentDict['aboutAuthor_blog'][0]))
+        QMessageBox.about(self, uilang.kMsgLanguageContentDict['aboutAuthor_title'][0], msgText )
+
+    def callbackShowRevisionHistory(self):
+        QMessageBox.about(self, uilang.kMsgLanguageContentDict['revisionHistory_title'][0], uilang.kMsgLanguageContentDict['revisionHistory_v1_0_0'][0] )
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
