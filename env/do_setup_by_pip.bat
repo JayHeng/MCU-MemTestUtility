1683054410516827318f55b627ac604264e31bc3
Jay Heng <hengjie1989@foxmail.com>
2024-07-25 17:00:45 +0800

    [ENV] Correct matplotlib version
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- env/do_setup_by_pip.bat
+++ env/do_setup_by_pip.bat
@@ -2,7 +2,7 @@ pip.exe install PyQt5==5.15.1
 pip.exe install pyqt5-tools==5.15.1.3
 pip.exe install qt5-applications==5.15.1.2 
 pip.exe install pyserial==3.5
-pip.exe install matplotlib==3.3.3
+pip.exe install matplotlib==3.5.3
 pip.exe install crc==1.2.0
 
 pip.exe install PyInstaller==4.2
