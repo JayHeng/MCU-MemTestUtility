d2bc4183c448cf3db71816836c94d36da38bad06
Jay Heng <hengjie1989@foxmail.com>
2024-09-26 12:59:10 +0800

    Can set flexspi connection configuration #3
    - Added complete flexspi2 pin option defn for RT1180
    
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- src/ui/ui_def_flexspi_conn_rt1180.py
+++ src/ui/ui_def_flexspi_conn_rt1180.py
@@ -14,7 +14,11 @@ kFlexspiConnSel_DataL4b = [
                               {'PortA_DATA[3:0] - GPIO_B2[13:10]':0x00,
                                'PortB_DATA[3:0] - GPIO_SD_B2[11:8]':0x10
                               },
-                              {'None':0xFF
+                              {'PortA_DATA[3:0] - GPIO_EMC_B1[38:35]':0x00,
+                               'PortA_DATA[3:0] - GPIO_AON[27:24]':0x01,
+                               'PortB_DATA[3:0] - GPIO_EMC_B1[33:30]':0x10,
+                               'PortB_DATA[3:0] - GPIO_AON[18:15]':0x11,
+                               'PortB_DATA[3:0] - GPIO_EMC_B1[25:22]':0x12,
                               }
                           ]
 
@@ -23,7 +27,10 @@ kFlexspiConnSel_DataH4b = [
                                'PortB_DATA[7:4] - GPIO_SD_B2[3:0]':0x11,
                                'None':0xFF
                               },
-                              {'None':0xFF
+                              {'PortB_DATA[3:0] - GPIO_EMC_B1[33:30]':0x10,
+                               'PortB_DATA[3:0] - GPIO_AON[18:15]':0x11,
+                               'PortB_DATA[3:0] - GPIO_EMC_B1[25:22]':0x12,
+                               'None':0xFF
                               }
                           ]
 
@@ -36,9 +43,17 @@ kFlexspiConnSel_DataT8b    = [
 
 kFlexspiConnSel_ssb     = [
                               {'PortA_SS0_B - GPIO_B2[9]':0x00,
-                               'PortB_SS0_B - GPIO_SD_B2[6]':0x10
+                               'PortB_SS0_B - GPIO_SD_B2[6]':0x10,
+                               'PortB_SS0_B - GPIO_SD_B1[4]':0x11
                               },
-                              {'None':0xFF
+                              {'PortA_SS0_B - GPIO_EMC_B1[39]':0x00,
+                               'PortA_SS0_B - GPIO_AON[22]':0x01,
+                               'PortA_SS1_B - GPIO_EMC_B1[26]':0x02,
+                               'PortA_SS1_B - GPIO_AON[20]':0x03,
+                               'PortA_SS1_B - GPIO_AON[19]':0x04,
+                               'PortB_SS0_B - GPIO_EMC_B1[28]':0x10,
+                               'PortB_SS0_B - GPIO_AON[21]':0x11,
+                               'PortB_SS1_B - GPIO_EMC_B1[27]':0x12,
                               }
                           ]
 
@@ -46,7 +61,10 @@ kFlexspiConnSel_sclk    = [
                               {'PortA_SCLK - GPIO_B2[8]':0x00,
                                'PortB_SCLK - GPIO_SD_B2[7]':0x10
                               },
-                              {'None':0xFF
+                              {'PortA_SCLK - GPIO_EMC_B1[41]':0x00,
+                               'PortA_SCLK - GPIO_AON[23]':0x01,
+                               'PortB_SCLK - GPIO_EMC_B1[34]':0x10,
+                               'PortB_SCLK - GPIO_AON[19]':0x11,
                               }
                           ]
 
@@ -54,14 +72,23 @@ kFlexspiConnSel_dqs     = [
                               {'PortA_DQS - GPIO_B2[7]':0x00,
                                'PortB_DQS - GPIO_SD_B2[5]':0x10
                               },
-                              {'None':0xFF
+                              {'PortA_DQS - GPIO_EMC_B1[40]':0x00,
+                               'PortA_DQS - GPIO_AON[21]':0x01,
+                               'PortA_DQS - GPIO_AON[28]':0x02,
+                               'PortB_DQS - GPIO_EMC_B1[29]':0x10,
+                               'PortB_DQS - GPIO_AON[20]':0x11,
+                               'PortB_DQS - GPIO_AON[28]':0x12,
+                               'PortB_DQS - GPIO_EMC_B1[21]':0x13,
+                               'None':0xFF
                               }
                           ]
 
 kFlexspiConnSel_sclkn   = [
                               {'None':0xFF
                               },
-                              {'None':0xFF
+                              {'PortB_SCLK - GPIO_EMC_B1[34]':0x10,
+                               'PortB_SCLK - GPIO_AON[19]':0x11,
+                               'None':0xFF
                               }
                           ]
 
