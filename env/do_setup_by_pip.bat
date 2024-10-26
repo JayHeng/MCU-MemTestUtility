a22d2fb13b92444cb0434810ff2c3d3b5e477494
Jay Heng <hengjie1989@foxmail.com>
2024-10-26 21:47:46 +0800

    [ENV] Switch to python 3.11
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- env/do_setup_by_pip.bat
+++ env/do_setup_by_pip.bat
@@ -1,8 +1,8 @@
-pip.exe install PyQt5==5.15.1 
-pip.exe install pyqt5-tools==5.15.1.3
-pip.exe install qt5-applications==5.15.1.2 
+pip.exe install PyQt5==5.15.9 
+pip.exe install pyqt5-tools==5.15.9.3.3
+pip.exe install qt5-applications==5.15.2.2.3
 pip.exe install pyserial==3.5
-pip.exe install matplotlib==3.5.3
+pip.exe install matplotlib==3.9.1
 pip.exe install crc==1.2.0
 
-pip.exe install PyInstaller==4.2
+pip.exe install PyInstaller==6.4.0
